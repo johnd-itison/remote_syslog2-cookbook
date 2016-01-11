b21bd06246621893391ae0c672a56bbb42d70beb
John Daniels <john.daniels@itison.com>
2016-01-11 17:31:53 +0000

    Support alternative install from deb packages.
    The Problem:
    ============
    We need to be able to install newer versions of remote_system2 due to
    issues with Papertrail's SSL certs (see:
    http://blog.papertrailapp.com/logs-papertrailapp-com-tls-ssl-cert-will-change-january-27/)
    
    The Solution:
    =============
    Since remote_syslog2's binary doesn't have a -v/--version option to give
    the installed version, switch to using the provided OS packages, which
    will allow us to track the installed version.
    
    remote_syslog2 provide deb and rpm packages, but I've only added deb
    support specifically for Debian and Ubuntu.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,11 +10,23 @@ default['remote_syslog2']['config'] = {
   }
 }
 
+case node['platform']
+when 'ubuntu', 'debian'
+  install_method = "deb"
+else
+  install_method = "source"
+end
+
+default['remote_syslog2']['install']['method'] = install_method
+
 # These attributes probably shouldn't be changed unless they specifically need to be
 default['remote_syslog2']['config_file'] = '/etc/remote_syslog2.yml'
 default['remote_syslog2']['pid_dir'] = '/var/run'
-default['remote_syslog2']['install']['download_file'] = 'https://github.com/papertrail/remote_syslog2/releases/download/v0.13/remote_syslog_linux_386.tar.gz'
-default['remote_syslog2']['install']['download_path'] = '/tmp/remote_syslog.tar.gz'
+default['remote_syslog2']['install']['version'] = '0.16'
+default['remote_syslog2']['install']['download_tarball'] = "https://github.com/papertrail/remote_syslog2/releases/download/v#{node['remote_syslog2']['install']['version']}/remote_syslog_linux_386.tar.gz"
+default['remote_syslog2']['install']['download_deb'] = "https://github.com/papertrail/remote_syslog2/releases/download/v#{node['remote_syslog2']['install']['version']}-beta-pkgs/remote-syslog2_#{node['remote_syslog2']['install']['version']}_i386.deb"
+default['remote_syslog2']['install']['download_tarball_path'] = '/tmp/remote_syslog.tar.gz'
+default['remote_syslog2']['install']['download_deb_path'] = '/tmp/remote_syslog2.deb'
 default['remote_syslog2']['install']['extract_path'] = '/tmp'
 default['remote_syslog2']['install']['extracted_path'] = '/tmp/remote_syslog'
 default['remote_syslog2']['install']['extracted_bin'] = 'remote_syslog'
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,25 +1,51 @@
 install = node['remote_syslog2']['install']
 bin_file = "#{install['bin_path']}/#{install['bin']}"
 
-remote_file install['download_path'] do
-  source install['download_file']
-  mode '0644'
-  not_if { ::File.exists?(bin_file) }
-end
+if install['method'] == "source"
 
-bash 'extract remote_syslog2' do
-  cwd '/tmp'
-  code <<-EOH
+  remote_file install['download_tarball_path'] do
+    source install['download_tarball']
+    mode '0644'
+    not_if { ::File.exists?(bin_file) }
+  end
+
+  bash 'extract remote_syslog2' do
+    cwd '/tmp'
+    code <<-EOH
     mkdir -p #{install['extracted_path']}
-    tar xzf #{install['download_path']} -C #{install['extract_path']}
+    tar xzf #{install['download_tarball_path']} -C #{install['extract_path']}
     mv #{install['extracted_path']}/#{install['extracted_bin']} #{bin_file}
-    rm -rf #{install['download_path']} #{install['extracted_path']}
-  EOH
-  not_if { ::File.exists?(bin_file) }
-end
+    rm -rf #{install['download_tarball_path']} #{install['extracted_path']}
+    EOH
+    not_if { ::File.exists?(bin_file) }
+  end
 
-file bin_file do
-  user 'root'
-  group 'root'
-  mode 0755
+  file bin_file do
+    user 'root'
+    group 'root'
+    mode 0755
+  end
+elsif install['method'] == "deb"
+  dpkg_package "remote_syslog2" do
+    action :nothing
+    source install['download_deb_path']
+  end
+
+  remote_file install['download_deb_path'] do
+    source install['download_deb']
+    mode '0644'
+    # This looks messy, but it's just checking that the installed version (if any) isn't the
+    # same as the version we want to install. This allows us to do upgrades just by changing
+    # an attribute.
+    not_if "test `dpkg-query -W --showformat='${Version}' remote-syslog2:i386` = #{install['version']}"
+    notifies :install, "dpkg_page[remote_syslog2]", :immediately
+  end
+
+
+  # Remove legacy binary if previously installed using 'source' method.
+
+  file bin_file do
+    action :delete
+    only_if { ::File.exists?(bin_file) }
+  end
 end
