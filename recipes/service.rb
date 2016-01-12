4e16b70bdcf7682157e3f26b8d69fe7dc1e59c26
John Daniels <john.daniels@itison.com>
2016-01-12 10:43:53 +0000

    Tidy up service for new install method options.

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,6 +1,14 @@
-template '/etc/init.d/remote_syslog2' do
-  source 'remote_syslog2.erb'
-  mode '0755'
+if node['install']['method'] == "source"
+  template '/etc/init.d/remote_syslog2' do
+    source 'remote_syslog2.erb'
+    mode '0755'
+  end
+else
+  # Clean up old 'source'/'tarball' type installations.
+  file "/etc/init.d/remote_syslog2" do
+    action :delete
+    only_if { ::File.exists?("/etc/init.d/remote_syslog2" }
+  end
 end
 
 service 'remote_syslog' do
